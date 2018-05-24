cbf43b08cbb0f1daae8357260d88b259d945294d
PuddingPudding <hwsh70119@gmail.com>
2018-05-24 17:26:00 +0800

    初次正式commit

---
+++ Assets/Draggable.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    public Transform parentToReturnTo = null;
+    public Transform placeholderParent = null;
+
+    GameObject placeholder = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    { //剛被拖曳時觸發
+        Debug.Log("OnBeginDrag");
+
+        placeholder = new GameObject();
+        placeholder.transform.SetParent(this.transform.parent);
+        LayoutElement le = placeholder.AddComponent<LayoutElement>();
+        le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+        le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+        le.flexibleWidth = 0;
+        le.flexibleHeight = 0;
+
+        placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+
+        parentToReturnTo = this.transform.parent;
+        placeholderParent = parentToReturnTo;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false; //由於在拖曳期間滑鼠對於畫面的raycast會被卡牌本身擋住，所以在拖曳期間先關閉
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    { //拖曳期間觸發
+        //Debug.Log ("OnDrag");
+
+        this.transform.position = eventData.position;
+
+        if (placeholder.transform.parent != placeholderParent)
+            placeholder.transform.SetParent(placeholderParent);
+
+        int newSiblingIndex = placeholderParent.childCount;
+
+        for (int i = 0; i < placeholderParent.childCount; i++)
+        {
+            if (this.transform.position.x < placeholderParent.GetChild(i).position.x) //從最小數過來，當我拖曳著的卡牌位子比對應位子的卡牌右邊些的話
+            {
+
+                newSiblingIndex = i; //那就把我預設放下後要歸位的位子改為目前的這一個
+
+                if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+                    newSiblingIndex--;
+
+                break;
+            }
+        }
+
+        placeholder.transform.SetSiblingIndex(newSiblingIndex);
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        this.transform.SetParent(parentToReturnTo);
+        this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        Destroy(placeholder);
+    }
+
+
+
+}
